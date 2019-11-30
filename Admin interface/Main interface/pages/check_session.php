a8a4594a270113347db7889f523621b3f900f07e
malekbk98 <47837824+malekbk98@users.noreply.github.com>
2019-11-30 20:07:37 +0100

    Add session & update top nav bar

--- Admin interface/Main interface/pages/check_session.php
+++ Admin interface/Main interface/pages/check_session.php
@@ -2,6 +2,13 @@
 session_start();
 if(!isset($_SESSION['log'])){
   header("Location: 404.html");
+}else{
+  include 'dbconnexion.php';
+  $id=$_SESSION['log'];
+  $req= $conx->query("SELECT * From employee where eid='$id'");
+  $data = $req->fetch();
+  $ad_name=$data['name'];
+  $ad_pic=$data['pic'];
 }
-echo $_SESSION['log'];
+
 ?>
