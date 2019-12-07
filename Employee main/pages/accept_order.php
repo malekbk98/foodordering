056c7e6d2584d1ba970937202f86bcf6c038561c
Amine <aminebenslama10@gmail.com>
2019-12-07 19:22:10 +0100

    add delivery

---
+++ Employee main/pages/accept_order.php
@@ -0,0 +1,15 @@
+<?php
+       $id=$_GET['id'];
+       $status=$_GET['status'];
+       include 'dbconnexion.php'; 
+       if($status=1){
+              $req = $conx->prepare("UPDATE orders SET status=2 WHERE oid=:param_oid");
+              $req->bindParam(':param_oid',$id);
+       }else{
+       
+              $req = $conx->prepare("UPDATE orders SET status=1 WHERE oid=:param_oid");
+       }
+              $req->bindParam(':param_oid',$id);
+       $req->execute();
+       header("Location: employee_order.php?msg=OrderAccepted");
+?>       
